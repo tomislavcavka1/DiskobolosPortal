276de1f31f346b0a972168c185693db2b4662f25
tihomirCavka <tihomir.cavka1@gmail.com>
2017-06-05 17:36:45 +0200

    Tihomir's change with conflicts

--- src/main/webapp/js/diskobolos/membershipCategory/membershipCategoryModule.js
+++ src/main/webapp/js/diskobolos/membershipCategory/membershipCategoryModule.js
@@ -12,28 +12,103 @@ membershipCategoryModule.controller('membershpCategoryController', function (
         MembershipCategoryDataFactory,
         $uibModal,
         DTOptionsBuilder,
+        DTColumnDefBuilder,
         SweetAlert,
         toaster) {
                                     
-      $scope.dtOptions = DTOptionsBuilder.newOptions()
-        .withDOM('<"html5buttons"B>lTfgitp')
-        .withButtons([
-            {extend: 'copy'},
-            {extend: 'csv'},
-            {extend: 'excel', title: 'ExampleFile'},
-            {extend: 'pdf', title: 'ExampleFile'},
+       var language = {
+        "sEmptyTable": "Nema podataka u tablici",
+        "sInfo": "Prikazano _START_ do _END_ od _TOTAL_ rezultata",
+        "sInfoEmpty": "Prikazano 0 do 0 od 0 rezultata",
+        "sInfoFiltered": "(filtrirano iz _MAX_ ukupnih rezultata)",
+        "sInfoPostFix": "",
+        "sInfoThousands": ",",
+        "sLengthMenu": "Prikaži _MENU_ rezultata po stranici",
+        "sLoadingRecords": "Dohvaćam...",
+        "sProcessing": "Obrađujem...",
+        "sSearch": "Pretraži:",
+        "sZeroRecords": "Ništa nije pronađeno",
+        "oPaginate": {
+            "sFirst": "Prva",
+            "sPrevious": "Nazad",
+            "sNext": "Naprijed",
+            "sLast": "Zadnja"
+        },
+        "oAria": {
+            "sSortAscending": ": aktiviraj za rastući poredak",
+            "sSortDescending": ": aktiviraj za padajući poredak"
+        }
+    }
 
-            {extend: 'print',
-                customize: function (win){
-                    $(win.document.body).addClass('white-bg');
-                    $(win.document.body).css('font-size', '10px');
 
-                    $(win.document.body).find('table')
-                        .addClass('compact')
-                        .css('font-size', 'inherit');
+
+
+    $scope.dtOptions = DTOptionsBuilder.newOptions()
+            .withDOM('<"html5buttons"B>lTfgitp')
+            .withLanguage(language)
+            .withOption('order', [0, 'asc'])
+            .withPaginationType('full_numbers')
+            .withButtons([
+                {extend: 'selectAll', text: 'Selektiraj sva polja', exportOptions: {
+                        columns: ':visible:not(.not-export-col)', modifier: {
+                            selected: true
+                        }
+                    }},
+                {extend: 'selectNone', text: 'Ukloni selektirana polja', exportOptions: {
+                        columns: ':visible:not(.not-export-col)', modifier: {
+                            selected: true
+                        }
+                    }},
+                {extend: 'colvis', text: '<i class="fa fa-list-ul" aria-hidden="true"></i> Prikaz polja u tablici',
+                    columns: [1]},
+                {extend: 'copy', text: '<i class="fa fa-files-o"></i> Kopiraj', exportOptions: {
+                        columns: ':visible:not(.not-export-col)', modifier: {
+                            selected: true
+                        }
+                    }},
+                {extend: 'csv', text: '<i class="fa fa-file-text-o"></i> CSV', exportOptions: {
+                        columns: ':visible:not(.not-export-col)', modifier: {
+                            selected: true
+                        }
+                    }},
+                {extend: 'excel', text: '<i class="fa fa-file-excel-o"></i> Excel', exportOptions: {
+                        columns: ':visible:not(.not-export-col)', modifier: {
+                            selected: true
+                        }
+                    }},
+                {extend: 'pdf', text: '<i class="fa fa-file-pdf-o"></i> PDF', exportOptions: {
+                        columns: ':visible:not(.not-export-col)', modifier: {
+                            selected: true
+                        }
+                    }},
+                {extend: 'print', text: '<i class="fa fa-print"></i> Ispis', exportOptions: {
+                        columns: ':visible:not(.not-export-col)', modifier: {
+                            selected: true
+                        }
+                    },
+                    customize: function (win) {
+                        $(win.document.body).addClass('white-bg');
+                        $(win.document.body).css('font-size', '10px');
+
+                        $(win.document.body).find('table')
+                                .addClass('compact')
+                                .css('font-size', 'inherit');
+                    }
                 }
-            }
-        ]);
+            ])
+            .withOption(
+                    'select', true
+                    )
+
+            .withOption(
+                    'responsive', true
+                    )
+            ;
+
+    $scope.dtColumnDefs = [
+        DTColumnDefBuilder.newColumnDef(-1).withOption('responsivePriority', 1)
+
+    ];
         
       $scope.$on('membershipCategories', function (ev, membershipCategories) {
         $scope.membershipCategories = membershipCategories;
