7e8a7f9b62749d2b5431a160c170340fe9b124cb
tihomirCavka <tihomir.cavka1@gmail.com>
2017-07-19 16:51:02 +0200

    new module ( grid datatable option )

--- src/main/webapp/js/diskobolos/newModule/newModule.js
+++ src/main/webapp/js/diskobolos/newModule/newModule.js
@@ -1,3 +1,5 @@
+/* global app */
+
 /**
  * AngularJS controller responsible for fetching, creation, edit and deletion of the membership category data.
  * 
@@ -8,56 +10,12 @@ var newModule = angular.module('newModule', []);
 newModule.controller('newModuleController',
         function Ctrl($scope, DTOptionsBuilder,
                 DTColumnDefBuilder, dataTableUtils) {
-            $scope.model = {
-                contacts: [{
-                        id: 1,
-                        name: "Lorem",
-                        number1: 28,
-                        number2: 28
-                    }, {
-                        id: 2,
-                        name: "Lorem",
-                        number1: 12,
-                        number2: 12
-                    }, {
-                        id: 3,
-                        name: "Lorem",
-                        number1: 34,
-                        number2: 34
-                    }, {
-                        id: 4,
-                        name: "Lorem",
-                        number1: 56,
-                        number2: 56
-                    }],
-                selected: {}
-            };
-
-            // gets the template to ng-include for a table row / item
-            $scope.getTemplate = function (contact) {
-                if (contact.id === $scope.model.selected.id)
-                    return 'edit';
-                else
-                    return 'display';
-            };
-            
-         
-
-            $scope.editContact = function (contact) {
-                $scope.model.selected = angular.copy(contact);
-            };
-                      
-
-
-            $scope.saveContact = function (idx) {
-                console.log("Saving contact");
-                $scope.model.contacts[idx] = angular.copy($scope.model.selected);
-                $scope.reset();
-            };
-
-            $scope.reset = function () {
-                $scope.model.selected = {};
-            };
+            $scope.contacts = [
+                {'id': '0' , 'RB' : '1', 'clanice':'clanica 1','sredstva_udruge': '7.000,00', 'sredstva_sportovi':'10.000,00' },
+                {'id': '1' , 'RB' : '2', 'clanice':'clanica 2','sredstva_udruge': '6.000,00', 'sredstva_sportovi':'10.000,00' },
+                {'id': '2' , 'RB' : '3', 'clanice':'clanica 3','sredstva_udruge': '3.000,00', 'sredstva_sportovi':'10.000,00' },
+                {'id': '3' , 'RB' : '4', 'clanice':'clanica 4','sredstva_udruge': '5.000,00', 'sredstva_sportovi':'10.000,00' }
+            ];
 
             $scope.dtOptions = DTOptionsBuilder.newOptions()
                     .withDOM('<"html5buttons"B>lTfgitp')
@@ -72,5 +30,42 @@ newModule.controller('newModuleController',
                 DTColumnDefBuilder.newColumnDef(-1).withOption('responsivePriority', 1)
             ];
 
-
         });
+
+
+newModule.directive('editInPlace', function () {
+    return {
+        restrict: 'E',
+        scope: {value: '='},
+        template: '<span ng-click="edit()" ng-bind="value"></span><input ng-blur="onFocusLost()" ng-model="value"></input>',
+        link: function ($scope, element, attrs) {
+            // Let's get a reference to the input element, as we'll want to reference it.
+            var inputElement = angular.element(element.children()[1]);
+
+            // This directive should have a set class so we can style it.
+            element.addClass('edit-in-place');
+
+            // Initially, we're not editing.
+            $scope.editing = false;
+
+            // ng-click handler to activate edit-in-place
+            $scope.edit = function () {
+                $scope.editing = true;
+
+                // We control display through a class on the directive itself. See the CSS.
+                element.addClass('active');
+
+                // And we must focus the element. 
+                // `angular.element()` provides a chainable array, like jQuery so to access a native DOM function, 
+                // we have to reference the first element in the array.
+                inputElement[0].focus();
+            };
+           
+            // When we leave the input, we're done editing.
+            $scope.onFocusLost = function() {
+                $scope.editing = false;
+                element.removeClass('active');
+            };
+        }
+    };
+});
